343a8043e8f5de00cd41e9077457c966cc2402d0
Lorrayne <lorrayne@gmail.com>
2025-09-25 22:22:55 -0300

    recfactor routes to use AdminController for admin home route

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.admin')
+
+@section('conteudo')
+      <h1>Bem-vindo ao Lambida Doce!</h1>
+
+        <p>
+            Explore nossos deliciosos produtos e
+            aproveite ofertas exclusivas.
+        </p>
+
+        <a href="#">Admin</a>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
-
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+route::controller(AdminController::class)->group(function (){
+    Route::get('/admin', 'home')->name('admin.home');
 });
